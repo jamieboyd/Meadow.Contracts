d7ce2b01cef075174f3a59df51e9494b2761d615
Adrian Stevens <adrian.stevens@gmail.com>
2022-04-24 15:55:30 -0700

    Update joystick interfaces and supporting classes

---
+++ Source/Meadow.Contracts/Peripherals/Sensors/Hid/IAnalogJoystick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Meadow.Peripherals.Sensors.Hid
+{
+    /// <summary>
+    /// Interface describing 2-axis analog joysticks.
+    /// </summary>
+    public interface IAnalogJoystick
+    {
+        /// <summary>
+        /// Is the horizontal / x-axis inverted 
+        /// </summary>
+        public bool IsHorizontalInverted { get; protected set; }
+
+        /// <summary>
+        /// Is the horizontal / x-axis inverted 
+        /// </summary>
+        public bool IsVerticalInverted { get; protected set; }
+
+        /// <summary>
+        /// Current position of
+        /// </summary>
+        public AnalogJoystickPosition? Position { get; protected set; }
+
+        /// <summary>
+        /// Raised when a new reading has been made. 
+        /// </summary>
+        event EventHandler<ChangeResult<AnalogJoystickPosition>> Updated;
+    }
+}
