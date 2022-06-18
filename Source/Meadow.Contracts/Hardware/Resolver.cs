4b1dd5920ba1698455e826a9816e31f66cfbcb55
ctacke <ctacke@gmail.com>
2022-06-18 13:17:37 -0500

    added resolver and logging

---
+++ Source/Meadow.Contracts/Hardware/Resolver.cs
@@ -0,0 +1,20 @@
+﻿using Meadow.Logging;
+
+namespace Meadow
+{
+    public static class Resolver 
+    {
+        public static ServiceCollection Services { get; }
+
+        static Resolver()
+        {
+            Services = new ServiceCollection();
+            Services.Add(Services);
+        }
+
+        public static Logger Log
+        {
+            get => Services?.Get<Logger>();
+        }
+    }
+}
