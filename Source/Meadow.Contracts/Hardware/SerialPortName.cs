3ba3238fcbfee2b54e1778ea4eaa197bc5fab631
ctacke <ctacke@gmail.com>
2022-09-07 21:10:49 -0500

    core compute v2

--- Source/Meadow.Contracts/Hardware/SerialPortName.cs
+++ Source/Meadow.Contracts/Hardware/SerialPortName.cs
@@ -1,10 +1,14 @@
-﻿using System;
-namespace Meadow.Hardware
+﻿namespace Meadow.Hardware
 {
     public class SerialPortName
     {
         public string FriendlyName { get; set; }
         public string SystemName { get; set; }
+
+        public static SerialPortName Create(string systemName)
+        {
+            return new SerialPortName("manually created", systemName);
+        }
 
         public SerialPortName(string friendlyName, string systemName)
         {
