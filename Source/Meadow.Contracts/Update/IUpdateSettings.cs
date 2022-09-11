e4fa838520ea382b344e16c58703cae7167b0de2
ctacke <ctacke@gmail.com>
2022-09-10 19:53:08 -0500

    updated device info

--- Source/Meadow.Contracts/Update/IUpdateSettings.cs
+++ Source/Meadow.Contracts/Update/IUpdateSettings.cs
@@ -5,7 +5,6 @@
         public bool Enabled { get; }
         public string UpdateServer { get; }
         public int UpdatePort { get; }
-        public string ClientID { get; }
         public string RootTopic { get; }
         public int CloudConnectRetrySeconds { get; }
     }
