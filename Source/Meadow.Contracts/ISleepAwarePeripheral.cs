12d6c42b862c299603551db18a60a47c63f33d6f
Chris Tacke <ctacke@opennetcf.com>
2022-10-27 11:58:15 -0500

    Added ISleepAwarePeripheral

---
+++ Source/Meadow.Contracts/ISleepAwarePeripheral.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meadow
+{
+    public interface ISleepAwarePeripheral
+    {
+        public Task BeforeSleep(CancellationToken cancellationToken);
+        public Task AfterWake(CancellationToken cancellationToken);
+    }
+}
