8eca0e9f3e3994ca71038d7b6a51743751a2012e
Chris Tacke <ctacke@opennetcf.com>
2022-11-21 12:24:06 -0600

    Added sd support

--- Source/Meadow.Contracts/IPlatformOS.Configuration.cs
+++ Source/Meadow.Contracts/IPlatformOS.Configuration.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Meadow
+﻿namespace Meadow
 {
     public partial interface IPlatformOS
     {
@@ -30,7 +29,8 @@ namespace Meadow
             ResetReason,
             RebootOnUnhandledException,
             InitializationTimeout,
-            SdCardPresent
+            SdCardEnabled,
+            SdCardMountPoint,
         };
 
         T GetConfigurationValue<T>(ConfigurationValues item) where T : struct;
@@ -43,6 +43,5 @@ namespace Meadow
         string MonoVersion { get; }
         bool RebootOnUnhandledException { get; }
         uint InitializationTimeout { get; }
-        bool SdCardPresent { get; }
     }
 }
--- Source/Meadow.Contracts/IPlatformOS.cs
+++ Source/Meadow.Contracts/IPlatformOS.cs
@@ -8,10 +8,25 @@ namespace Meadow
     /// </summary>
     public partial interface IPlatformOS : IPowerController
     {
+        /// <summary>
+        /// Initializes platform-specific OS features
+        /// </summary>
         void Initialize();
 
+        /// <summary>
+        /// Gets the current CPU temperature
+        /// </summary>
+        /// <returns></returns>
         Temperature GetCpuTemperature();
 
+        /// <summary>
+        /// Gets the OS INtpClient instance
+        /// </summary>
         public INtpClient NtpClient { get; }
+
+        /// <summary>
+        /// Returns <b>true</b> if an SD storage card is detected and mounted
+        /// </summary>
+        bool SdCardPresent { get; }
     }
 }
