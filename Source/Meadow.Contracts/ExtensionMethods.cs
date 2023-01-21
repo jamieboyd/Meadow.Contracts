81a9cd1819196036a942a58e848e11d9bc8fbe16
Adrian Stevens <adrian.stevens@gmail.com>
2023-01-21 15:16:29 -0800

    Add circular and message buffer + minor cleanup

--- Source/Meadow.Contracts/ExtensionMethods.cs
+++ Source/Meadow.Contracts/ExtensionMethods.cs
@@ -16,33 +16,6 @@ namespace Meadow
         static public bool Contains<TSource>(this IEnumerable<TSource> source, TSource[] pattern)
         {
             return (source.FirstIndexOf(pattern) != -1);
-
-            //int patternLength = pattern.Length;
-            //int totalLength = source.Count();
-            //TSource firstMatch = pattern[0];
-
-            //for (int i = 0; i < totalLength; i++) {
-            //    // is this the right equality?
-            //    if ( (firstMatch.Equals (source.ElementAt(i)) ) // begin match?
-            //         &&
-            //         (totalLength - i >= patternLength) // can match exist?
-            //       ) {
-            //        TSource[] matchTest = new TSource[patternLength];
-            //        // copy the potential match into the matchTest array.
-            //        // can't use .Skip() and .Take() because it will actually
-            //        // enumerate over stuff and can have side effects
-            //        for (int x = 0; x < patternLength; x++) {
-            //            matchTest[x] = source.ElementAt(i + x);
-            //        }
-            //        // if the pattern pulled from source matches our search pattern
-            //        // then the pattern exists.
-            //        if (matchTest.SequenceEqual(pattern)) {
-            //            return true;
-            //        }
-            //    }
-            //}
-            //// if we go here, no pattern match
-            //return false;
         }
 
         /// TODO: move this into the `CircularBuffer` class? or is it broadly applicable?
