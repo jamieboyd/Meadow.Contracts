f95943d98e2b19883326e1b7f5de76579e07ee9a
Chris Tacke <ctacke@opennetcf.com>
2023-01-29 12:30:38 -0600

    eliminate a few warnings

--- Source/Meadow.Contracts/Enums/SpiLineType.cs
+++ Source/Meadow.Contracts/Enums/SpiLineType.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 namespace Meadow.Hardware
 {
+    /// <summary>
+    /// Description of a SPI line function
+    /// </summary>
     [Flags]
     public enum SpiLineType
     {
+        /// <summary>
+        /// Not a SPI function
+        /// </summary>
         None = 0,
-        MOSI = 1 << 0,
-        MISO = 1 << 1,
+        /// <summary>
+        /// A SPI Controller-out Peripheral-in line
+        /// </summary>
+        COPI = 1 << 0,
+        /// <summary>
+        /// A SPI Controller-in Peripheral-out line
+        /// </summary>
+        CIPO = 1 << 1,
+        /// <summary>
+        /// A SPI Clock line
+        /// </summary>
         Clock = 1 << 2,
-        ChipSelect = 1 << 3
+        /// <summary>
+        /// A SPI Chip Select line
+        /// </summary>
+        ChipSelect = 1 << 3,
+
+        /// <summary>
+        /// Backward compatible alias for COPI
+        /// </summary>
+        MOSI = COPI,
+        /// <summary>
+        /// Backward compatible alias for CIPO
+        /// </summary>
+        MISO = CIPO
+
     }
 }
