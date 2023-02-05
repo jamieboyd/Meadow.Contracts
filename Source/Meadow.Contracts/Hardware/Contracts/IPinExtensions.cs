317333c5a34214629fdc729cc8aca77887dd42e0
Chris Tacke <ctacke@opennetcf.com>
2023-02-05 15:17:28 -0600

    Added IPin extensions

--- Source/Meadow.Contracts/Hardware/Contracts/IPinExtensions.cs
+++ Source/Meadow.Contracts/Hardware/Contracts/IPinExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Meadow.Units;
+using System;
 using System.Linq;
 
 namespace Meadow.Hardware
@@ -32,6 +33,66 @@ namespace Meadow.Hardware
         {
             var chan = pin.SupportedChannels.OfType<TChannel>().Where(where).FirstOrDefault();
             return chan != null;
+        }
+
+        public static IDigitalOutputPort CreateDigitalOutputPort(this IPin pin, bool initialState = false)
+        {
+            if (pin.Controller is IDigitalOutputController controller)
+            {
+                return controller.CreateDigitalOutputPort(pin, initialState);
+            }
+
+            throw new ArgumentException("Pin is not digital output capable");
+        }
+
+        public static IDigitalInputPort CreateDigitalInputPort(this IPin pin, InterruptMode interruptMode, ResistorMode resistorMode, TimeSpan debounceDuration, TimeSpan glitchDuration)
+        {
+            if (pin.Controller is IDigitalInputController controller)
+            {
+                return controller.CreateDigitalInputPort(pin, interruptMode, resistorMode, debounceDuration, glitchDuration);
+            }
+
+            throw new ArgumentException("Pin is not digital input capable");
+        }
+
+        public static IDigitalInputPort CreateDigitalInputPort(this IPin pin, InterruptMode interruptMode = InterruptMode.None, ResistorMode resistorMode = ResistorMode.Disabled)
+        {
+            if (pin.Controller is IDigitalInputController controller)
+            {
+                return controller.CreateDigitalInputPort(pin, interruptMode, resistorMode);
+            }
+
+            throw new ArgumentException("Pin is not digital input capable");
+        }
+
+        public static IAnalogInputPort CreateAnalogInputPort(this IPin pin, int sampleCount, TimeSpan sampleInterval, Units.Voltage referenceVoltage)
+        {
+            if (pin.Controller is IAnalogInputController controller)
+            {
+                return controller.CreateAnalogInputPort(pin, sampleCount, sampleInterval, referenceVoltage);
+            }
+
+            throw new ArgumentException("Pin is not analog input capable");
+        }
+
+        public static IAnalogInputPort CreateAnalogInputPort(this IPin pin, int sampleCount)
+        {
+            if (pin.Controller is IAnalogInputController controller)
+            {
+                return controller.CreateAnalogInputPort(pin, sampleCount);
+            }
+
+            throw new ArgumentException("Pin is not analog input capable");
+        }
+
+        public static IPwmPort CreatePwmPort(this IPin pin, Frequency frequency, float dutyCycle = 0.5f, bool invert = false)
+        {
+            if (pin.Controller is IPwmOutputController controller)
+            {
+                return controller.CreatePwmPort(pin, frequency, dutyCycle, invert);
+            }
+
+            throw new ArgumentException("Pin is not PWM output capable");
         }
     }
 }
