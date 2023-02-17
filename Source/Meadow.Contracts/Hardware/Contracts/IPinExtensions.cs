e5565945109fb9c8efa3051eb85efdeac900777e
Chris Tacke <ctacke@opennetcf.com>
2023-02-16 20:43:00 -0600

    Added CreateBiDirectionalPort to IPin extensions

--- Source/Meadow.Contracts/Hardware/Contracts/IPinExtensions.cs
+++ Source/Meadow.Contracts/Hardware/Contracts/IPinExtensions.cs
@@ -35,6 +35,13 @@ namespace Meadow.Hardware
             return chan != null;
         }
 
+        /// <summary>
+        /// Creates an IDigitalOutputPort on a capable IPin
+        /// </summary>
+        /// <param name="pin">The source pin</param>
+        /// <param name="initialState">Initial state of the port</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static IDigitalOutputPort CreateDigitalOutputPort(this IPin pin, bool initialState = false)
         {
             if (pin.Controller is IDigitalOutputController controller)
@@ -45,6 +52,16 @@ namespace Meadow.Hardware
             throw new ArgumentException("Pin is not digital output capable");
         }
 
+        /// <summary>
+        /// Creates an IDigitalInputPort on a capable IPin
+        /// </summary>
+        /// <param name="pin">The source pin</param>
+        /// <param name="interruptMode"></param>
+        /// <param name="resistorMode"></param>
+        /// <param name="debounceDuration"></param>
+        /// <param name="glitchDuration"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static IDigitalInputPort CreateDigitalInputPort(this IPin pin, InterruptMode interruptMode, ResistorMode resistorMode, TimeSpan debounceDuration, TimeSpan glitchDuration)
         {
             if (pin.Controller is IDigitalInputController controller)
@@ -55,6 +72,14 @@ namespace Meadow.Hardware
             throw new ArgumentException("Pin is not digital input capable");
         }
 
+        /// <summary>
+        /// Creates an IDigitalInputPort on a capable IPin
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <param name="interruptMode"></param>
+        /// <param name="resistorMode"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static IDigitalInputPort CreateDigitalInputPort(this IPin pin, InterruptMode interruptMode = InterruptMode.None, ResistorMode resistorMode = ResistorMode.Disabled)
         {
             if (pin.Controller is IDigitalInputController controller)
@@ -65,6 +90,15 @@ namespace Meadow.Hardware
             throw new ArgumentException("Pin is not digital input capable");
         }
 
+        /// <summary>
+        /// Creates an IAnalogInputPort on a capable IPin
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <param name="sampleCount"></param>
+        /// <param name="sampleInterval"></param>
+        /// <param name="referenceVoltage"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static IAnalogInputPort CreateAnalogInputPort(this IPin pin, int sampleCount, TimeSpan sampleInterval, Units.Voltage referenceVoltage)
         {
             if (pin.Controller is IAnalogInputController controller)
@@ -75,6 +109,13 @@ namespace Meadow.Hardware
             throw new ArgumentException("Pin is not analog input capable");
         }
 
+        /// <summary>
+        /// Creates an IAnalogInputPort on a capable IPin
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <param name="sampleCount"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static IAnalogInputPort CreateAnalogInputPort(this IPin pin, int sampleCount)
         {
             if (pin.Controller is IAnalogInputController controller)
@@ -85,6 +126,15 @@ namespace Meadow.Hardware
             throw new ArgumentException("Pin is not analog input capable");
         }
 
+        /// <summary>
+        /// Creates an IPwmPort on a capable IPin
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <param name="frequency"></param>
+        /// <param name="dutyCycle"></param>
+        /// <param name="invert"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static IPwmPort CreatePwmPort(this IPin pin, Frequency frequency, float dutyCycle = 0.5f, bool invert = false)
         {
             if (pin.Controller is IPwmOutputController controller)
@@ -93,6 +143,42 @@ namespace Meadow.Hardware
             }
 
             throw new ArgumentException("Pin is not PWM output capable");
+        }
+
+        /// <summary>
+        /// Creates an IBiDirectionalPort on a capable IPin
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <param name="initialState"></param>
+        /// <param name="interruptMode"></param>
+        /// <param name="resistorMode"></param>
+        /// <param name="initialDirection"></param>
+        /// <param name="debounceDuration"></param>
+        /// <param name="glitchDuration"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static IBiDirectionalPort CreateBiDirectionalPort(this IPin pin, bool initialState, InterruptMode interruptMode, ResistorMode resistorMode, PortDirectionType initialDirection, TimeSpan debounceDuration, TimeSpan glitchDuration)
+        {
+            if (pin.Controller is IBiDirectionalController controller)
+            {
+                return controller.CreateBiDirectionalPort(pin, initialState, interruptMode, resistorMode, initialDirection, debounceDuration, glitchDuration);
+            }
+
+            throw new ArgumentException("Pin controller is not an IBiDirectionalController");
+        }
+
+        /// <summary>
+        /// Creates an IBiDirectionalPort on a capable IPin
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <param name="initialState"></param>
+        /// <param name="interruptMode"></param>
+        /// <param name="resistorMode"></param>
+        /// <param name="initialDirection"></param>
+        /// <returns></returns>
+        public static IBiDirectionalPort CreateBiDirectionalPort(this IPin pin, bool initialState = false, InterruptMode interruptMode = InterruptMode.None, ResistorMode resistorMode = ResistorMode.Disabled, PortDirectionType initialDirection = PortDirectionType.Input)
+        {
+            return pin.CreateBiDirectionalPort(initialState, interruptMode, resistorMode, initialDirection, TimeSpan.Zero, TimeSpan.Zero);
         }
     }
 }
