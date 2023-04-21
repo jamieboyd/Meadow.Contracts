a46b1750d4bc15ce02f715fcffbcba0bb4441d52
Adrian Stevens <adrian.stevens@gmail.com>
2023-04-21 08:21:44 -0700

    Require SPI speed and bus fo ISpiPeripheral and dynamically set SPI bus on exchange

--- Source/Meadow.Contracts/Hardware/Contracts/PortsAndBuses/ISpiPeripheral.cs
+++ Source/Meadow.Contracts/Hardware/Contracts/PortsAndBuses/ISpiPeripheral.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using Meadow.Units;
+
 namespace Meadow.Hardware
 {
+    /// <summary>
+    /// SPI peripheral abstraction
+    /// </summary>
     public interface ISpiPeripheral : IByteCommunications
     {
+        /// <summary>
+        /// Chip select port
+        /// </summary>
         IDigitalOutputPort ChipSelect { get; }
+
+        /// <summary>
+        /// SPI bus
+        /// </summary>
         ISpiBus Bus { get; }
+
+        /// <summary>
+        /// SPI bus speed
+        /// </summary>
+        Frequency BusSpeed { get; set; }
+
+        /// <summary>
+        /// SPI bus mode
+        /// </summary>
+        SpiClockConfiguration.Mode BusMode { get; set; }
     }
 }
