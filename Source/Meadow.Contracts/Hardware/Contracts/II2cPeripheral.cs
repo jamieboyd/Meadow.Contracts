82e1f9ceb3ad4859af5c9ccf2901ad03f40d2ab7
Adrian Stevens <adrian.stevens@gmail.com>
2023-04-25 10:17:19 -0700

    Rename ISpiDevice to ISpiPeripheral and add II2cPeripheral

---
+++ Source/Meadow.Contracts/Hardware/Contracts/II2cPeripheral.cs
@@ -0,0 +1,13 @@
+﻿namespace Meadow.Hardware
+{
+    /// <summary>
+    /// Interface for a device/peripheral that communicates over I2C
+    /// </summary>
+    public interface II2cPeripheral
+    {
+        /// <summary>
+        /// The default I2C address for the peripheral
+        /// </summary>
+        byte I2cDefaultAddress { get; }
+    }
+}
