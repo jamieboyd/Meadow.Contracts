fe478f81b8f238441516191cdcf41d9083d0b98b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-05-03 12:56:47 -0700

    Update LED Interfaces

--- Source/Meadow.Contracts/Peripherals/Leds/ILed.cs
+++ Source/Meadow.Contracts/Peripherals/Leds/ILed.cs
@@ -1,4 +1,7 @@
-﻿namespace Meadow.Peripherals.Leds
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Meadow.Peripherals.Leds
 {
     /// <summary>
     /// Defines a simple Light Emitting Diode (LED).
@@ -10,5 +13,22 @@
         /// </summary>
         /// <value><c>true</c> if is on; otherwise, <c>false</c>.</value>
         bool IsOn { get; set; }
+
+        /// <summary>
+        /// Blink animation that turns the LED on (500ms) and off (500ms)
+        /// </summary>
+        Task StartBlink();
+
+        /// <summary>
+        /// Blink animation that turns the LED on and off based on the OnDuration and offDuration values in ms
+        /// </summary>
+        /// <param name="onDuration"></param>
+        /// <param name="offDuration"></param>
+        Task StartBlink(TimeSpan onDuration, TimeSpan offDuration);
+
+        /// <summary>
+        /// Stops blink animation.
+        /// </summary>
+        Task StopAnimation();
     }
 }
--- Source/Meadow.Contracts/Peripherals/Leds/IRgbLed.cs
+++ Source/Meadow.Contracts/Peripherals/Leds/IRgbLed.cs
@@ -8,7 +8,6 @@
         /// <summary>
         /// Gets or sets a value indicating whether the LED is on.
         /// </summary>
-        /// <value><c>true</c> if is on; otherwise, <c>false</c>.</value>
         bool IsOn { get; set; }
     }
 }
