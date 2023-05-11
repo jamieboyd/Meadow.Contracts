aaa93535cedce3f5d81eb85a1b1b1412c59ee642
Chris Tacke <ctacke@opennetcf.com>
2023-05-11 09:53:30 -0500

    separate interrupt capability from DigitalInputPortBase

---
+++ Source/Meadow.Contracts/Hardware/Bases/InterruptableDigitalInputPortBase.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meadow.Hardware;
+
+public abstract class InterruptableDigitalInputPortBase : DigitalInputPortBase, IDigitalInterruptPort
+{
+    private List<Unsubscriber> _unsubscribers = new List<Unsubscriber>();
+
+    /// <summary>
+    /// Occurs when the state is changed. To enable this, set the InterruptMode at construction
+    /// </summary>
+    public event EventHandler<DigitalPortResult> Changed = delegate { };
+
+    /// <summary>
+    /// Gets or sets a value indicating the type of interrupt monitoring this input.
+    /// </summary>
+    /// <value><c>true</c> if interrupt enabled; otherwise, <c>false</c>.</value>
+    public InterruptMode InterruptMode { get; protected set; }
+
+    /// <summary>
+    /// Gets or sets the debounce duration for the port
+    /// </summary>
+    public abstract TimeSpan DebounceDuration { get; set; }
+    /// <summary>
+    /// Gets or sets the glitch filter duration for the port
+    /// </summary>
+    public abstract TimeSpan GlitchDuration { get; set; }
+
+    /// <summary>
+    /// Constructor for the DigitalInputPortBase
+    /// </summary>
+    /// <param name="pin"></param>
+    /// <param name="channel"></param>
+    /// <param name="interruptMode"></param>
+    protected InterruptableDigitalInputPortBase(
+        IPin pin,
+        IDigitalChannelInfo channel,
+        InterruptMode interruptMode = InterruptMode.None
+        )
+        : base(pin, channel)
+    {
+        // TODO: check interrupt mode (i.e. if != none, make sure channel info agrees)
+        InterruptMode = interruptMode;
+    }
+
+    /// <summary>
+    /// Raises the Changed event and notifies all observers of a state change
+    /// </summary>
+    /// <param name="changeResult"></param>
+    protected void RaiseChangedAndNotify(DigitalPortResult changeResult)
+    {
+        Changed?.Invoke(this, changeResult);
+        Observers.ForEach(x => x.OnNext(changeResult));
+    }
+
+    /// <summary>
+    /// Adds a state observer to the port
+    /// </summary>
+    /// <param name="observer"></param>
+    /// <returns></returns>
+    public IDisposable Subscribe(IObserver<IChangeResult<DigitalState>> observer)
+    {
+        if (!Observers.Contains(observer)) Observers.Add(observer);
+        var u = new Unsubscriber(Observers, observer);
+        _unsubscribers.Add(u);
+        return u;
+    }
+
+    /// <summary>
+    /// Releases allocated port resources 
+    /// </summary>
+    /// <param name="disposing"></param>
+    protected override void Dispose(bool disposing)
+    {
+        foreach (var u in _unsubscribers)
+        {
+            u.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private class Unsubscriber : IDisposable
+    {
+        private List<IObserver<IChangeResult<DigitalState>>> _observers;
+        private IObserver<IChangeResult<DigitalState>> _observer;
+
+        public Unsubscriber(List<IObserver<IChangeResult<DigitalState>>> observers, IObserver<IChangeResult<DigitalState>> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (!(_observer == null)) _observers.Remove(_observer);
+        }
+    }
+}
--- Source/Meadow.Contracts/Hardware/Contracts/PortsAndBuses/IDigitalInputPort.cs
+++ Source/Meadow.Contracts/Hardware/Contracts/PortsAndBuses/IDigitalInputPort.cs
@@ -1,27 +1,12 @@
-﻿using System;
-
-namespace Meadow.Hardware
+﻿namespace Meadow.Hardware
 {
-    // TODO: is the interface inheritance backwards here? shouldn't `IDigitalInterruptPort`
-    // inherit from `IDigitalInputPort`?
     /// <summary>
     /// Contract for ports that are capable of reading digital input and raising
     /// events when state changes.
     /// </summary>
-    public interface IDigitalInputPort : IDigitalInterruptPort, IDigitalPort, IObservable<IChangeResult<DigitalState>>
+    public interface IDigitalInputPort : IDigitalPort
     {
         bool State { get; }
-        ResistorMode Resistor { get; set;  }
-
-
-        public static FilterableChangeObserver<DigitalState>
-            CreateObserver(
-                Action<IChangeResult<DigitalState>> handler,
-                Predicate<IChangeResult<DigitalState>>? filter = null)
-        {
-            return new FilterableChangeObserver<DigitalState>(
-                handler, filter);
-        }
-
+        ResistorMode Resistor { get; set; }
     }
 }
