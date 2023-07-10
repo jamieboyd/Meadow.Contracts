0eea8fc9299afa7ea8200f70d77c220d2e5e8e2d
Chris Tacke <ctacke@opennetcf.com>
2023-07-10 16:24:35 -0500

    added sets to update settings

--- Source/Meadow.Contracts/Update/IUpdateSettings.cs
+++ Source/Meadow.Contracts/Update/IUpdateSettings.cs
@@ -8,37 +8,37 @@ public interface IUpdateSettings
     /// <summary>
     /// Gets the desired enabled state of the service
     /// </summary>
-    public bool Enabled { get; }
+    public bool Enabled { get; set; }
     /// <summary>
     /// Gets the address of the Update (MQTT) server to use
     /// </summary>
-    public string UpdateServer { get; }
+    public string UpdateServer { get; set; }
     /// <summary>
     /// Gets the port of the Update (MQTT) server to use
     /// </summary>
-    public int UpdatePort { get; }
+    public int UpdatePort { get; set; }
     /// <summary>
     /// Gets the address of the authentication server to use
     /// </summary>
-    public string AuthServer { get; }
+    public string AuthServer { get; set; }
     /// <summary>
     /// Gets the port of the authentication server to use
     /// </summary>
-    public int AuthPort { get; }
+    public int AuthPort { get; set; }
     /// <summary>
     /// Gets the Organization the device is registered to
     /// </summary>
-    public string Organization { get; }
+    public string Organization { get; set; }
     /// <summary>
     /// Gets the root MQTT topic to subscribe to for updates
     /// </summary>
-    public string RootTopic { get; }
+    public string RootTopic { get; set; }
     /// <summary>
     /// Reconnect period used when a disconnection from the Update server occrs
     /// </summary>
-    public int CloudConnectRetrySeconds { get; }
+    public int CloudConnectRetrySeconds { get; set; }
     /// <summary>
     /// Gets the preference for using authentication when connecting to the Update server
     /// </summary>
-    public bool UseAuthentication { get; }
+    public bool UseAuthentication { get; set; }
 }
