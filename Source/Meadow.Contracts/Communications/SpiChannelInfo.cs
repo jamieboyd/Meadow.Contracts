f320a08f4ea9dd4b86ebdcec3ed6c4a04135f559
Adrian Stevens <adrian.stevens@gmail.com>
2023-07-13 11:08:09 -0700

    Fix merge issue

--- Source/Meadow.Contracts/Communications/SpiChannelInfo.cs
+++ Source/Meadow.Contracts/Communications/SpiChannelInfo.cs
@@ -30,32 +30,6 @@ public class SpiChannelInfo : DigitalChannelInfoBase, ISpiChannelInfo
             pullUpCapable: pullUpCapable,
             inverseLogic: false) //TODO: switch to C# 7.2+ to get rid of trailing names
     {
-        /// <summary>
-        /// Gets the supported line types of the SPI channel.
-        /// </summary>
-        public SpiLineType LineTypes { get; protected set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SpiChannelInfo"/> class with the specified parameters.
-        /// </summary>
-        /// <param name="name">The name of the SPI channel.</param>
-        /// <param name="lineTypes">The supported line types of the SPI channel.</param>
-        /// <param name="pullDownCapable">Indicates whether the channel is capable of pull-down.</param>
-        /// <param name="pullUpCapable">Indicates whether the channel is capable of pull-up.</param>
-        public SpiChannelInfo(string name,
-            SpiLineType lineTypes,
-            bool pullDownCapable = false,
-            bool pullUpCapable = false)
-            : base(
-                name,
-                inputCapable: true,
-                outputCapable: true,
-                interruptCapable: false, // ?? i mean, technically, yes, but will we have events?
-                pullDownCapable: pullDownCapable,
-                pullUpCapable: pullUpCapable,
-                inverseLogic: false) //TODO: switch to C# 7.2+ to get rid of trailing names
-        {
-            LineTypes = lineTypes;
-        }
+        LineTypes = lineTypes;
     }
 }
