c3417dafb965ba19af438154d17f68c1630a4e8e
Chris Tacke <ctacke@opennetcf.com>
2023-07-20 16:59:40 -0500

    namespace and docs fixes

--- Source/Meadow.Contracts/Update/UpdateType.cs
+++ Source/Meadow.Contracts/Update/UpdateType.cs
@@ -1,21 +1,20 @@
-﻿namespace Meadow.Update
+﻿namespace Meadow.Update;
+
+/// <summary>
+/// Represents the type of update.
+/// </summary>
+public enum UpdateType
 {
     /// <summary>
-    /// Represents the type of update.
+    /// Update for the bootloader.
     /// </summary>
-    public enum UpdateType
-    {
-        /// <summary>
-        /// Update for the bootloader.
-        /// </summary>
-        Bootloader,
-        /// <summary>
-        /// Update for the operating system.
-        /// </summary>
-        OS,
-        /// <summary>
-        /// Update for the application.
-        /// </summary>
-        Application
-    }
+    Bootloader,
+    /// <summary>
+    /// Update for the operating system.
+    /// </summary>
+    OS,
+    /// <summary>
+    /// Update for the application.
+    /// </summary>
+    Application
 }
