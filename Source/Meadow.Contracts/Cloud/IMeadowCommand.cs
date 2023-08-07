502a59a51fee7ad2c1473eb5d63f7b695d3fb37e
Steven Kuhn <109609+stevenkuhn@users.noreply.github.com>
2023-08-07 10:43:05 -0500

    Add contracts for subscribing to Meadow.Cloud commands (#128)
    This adds interface and class contracts for the command and control feature of Meadow.Cloud. The purpose of this feature is to allow a user to publish command messages to their Meadow devices and have their devices respond accordingly.

---
+++ Source/Meadow.Contracts/Cloud/IMeadowCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Meadow.Cloud;
+
+/// <summary>
+/// Represents a command that can be sent to a Meadow device through Meadow.Cloud.
+/// </summary>
+public interface IMeadowCommand { }
---
+++ Source/Meadow.Contracts/Cloud/MeadowCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meadow.Cloud;
+
+/// <summary>
+/// Represents a generic command that can be sent to a Meadow device through Meadow.Cloud.
+/// </summary>
+public class MeadowCommand
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MeadowCommand"/> class.
+    /// </summary>
+    /// <param name="commandName">The name of the command to execute.</param>
+    /// <param name="arguments">The arguments of the command.</param>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="commandName"/> is null, empty or whitespace.</exception>
+    public MeadowCommand(string commandName, IReadOnlyDictionary<string, object>? arguments = null)
+    {
+        if (string.IsNullOrWhiteSpace(commandName))
+        {
+            throw new ArgumentException($"'{nameof(commandName)}' cannot be null or whitespace.", nameof(commandName));
+        }
+
+        CommandName = commandName;
+        Arguments = arguments ?? new Dictionary<string, object>();
+    }
+
+    /// <summary>
+    /// Gets the name of the command.
+    /// </summary>
+    public string CommandName { get; }
+
+    /// <summary>
+    /// Gets the arguments of the command.
+    /// </summary>
+    public IReadOnlyDictionary<string, object> Arguments { get; }
+}
