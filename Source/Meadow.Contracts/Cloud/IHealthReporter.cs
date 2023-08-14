a9baa451fc21288c7f10e56c9ba4f2d59b0c0215
Brian Kim <brian.kim@wildernesslabs.co>
2023-08-14 09:09:41 -0700

    add xml commands

--- Source/Meadow.Contracts/Cloud/IHealthReporter.cs
+++ Source/Meadow.Contracts/Cloud/IHealthReporter.cs
@@ -2,7 +2,14 @@ using System.Threading.Tasks;
 
 namespace Meadow.Cloud;
 
+/// <summary>
+/// Logic responsible for reporting device health metrics to Meadow.Cloud.
+/// </summary>
 public interface IHealthReporter
 {
+    /// <summary>
+    /// Starts the health reporter based on the desired interval.
+    /// </summary>
+    /// <param name="interval">In minutes</param>
     void Start(int interval);
 }
