f635db3d88ddfd6a0ff454de6ce6c6beaa5c318d
Chris Tacke <ctacke@opennetcf.com>
2023-08-25 10:19:56 -0500

    added xml docs

--- Source/Meadow.Contracts/Cloud/CloudEvent.cs
+++ Source/Meadow.Contracts/Cloud/CloudEvent.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 
 namespace Meadow.Cloud;
 
+/// <summary>
+/// An event message set to Meadow.Cloud
+/// </summary>
 public class CloudEvent
 {
+    /// <summary>
+    /// Gets or sets the CloudEvent's unique identifier
+    /// </summary>
     public int EventId { get; set; } = default!;
+    /// <summary>
+    /// Gets or sets the CloudEvent's Description
+    /// </summary>
     public string Description { get; set; } = default!;
+    /// <summary>
+    /// Gets or set the list of measurements associated with the CloudEvent
+    /// </summary>
     public Dictionary<string, object> Measurements { get; set; } = default!;
-    public DateTime Timestamp { get; set; } = default!;
+    /// <summary>
+    /// Gets or set the UTC timestamp when the CloudEvent was generated
+    /// </summary>
+    public DateTimeOffset Timestamp { get; set; } = default!;
 }
