61cb3a88832cdaec80c07a782d96101d2e594b22
Adrian Stevens <adrian.stevens@gmail.com>
2023-09-11 18:04:34 -0700

    Add IGnssSensor interface

---
+++ Source/Meadow.Contracts/Peripherals/Sensors/Location/Gnss/IGnssSensor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Meadow.Peripherals.Sensors.Location.Gnss
+{
+    /// <summary>
+    /// Interface describing a Global navigation satellite system (GNSS) sensor
+    /// </summary>
+    public interface IGnssSensor
+    {
+        /// <summary>
+        /// Raised when new GNSS data is available
+        /// </summary>
+        public event EventHandler<IGnssResult> GnssDataReceived;
+
+        /// <summary>
+        /// Supported GNSS result types
+        /// </summary>
+        public IGnssResult[] SupportedResultTypes { get; }
+
+        /// <summary>
+        /// Start updating GNSS data
+        /// </summary>
+        public void StartUpdating();
+
+        /// <summary>
+        /// Stop updating GNSS data
+        /// </summary>
+        public void StopUpdating();
+    }
+}
