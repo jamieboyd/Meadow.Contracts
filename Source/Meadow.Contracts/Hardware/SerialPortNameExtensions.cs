2e6ed0fa6b47d3930d0e555d23a4e610b970f887
Branden Gunn <engunneer@gmail.com>
2023-10-13 22:58:19 -0400

    xmldoc

--- Source/Meadow.Contracts/Hardware/SerialPortNameExtensions.cs
+++ Source/Meadow.Contracts/Hardware/SerialPortNameExtensions.cs
@@ -9,7 +9,7 @@ namespace Meadow
     public static class SerialPortNameExtensions
     {
         /// <summary>
-        /// Creates an ISerialPort directly from a SerialPortName using the current IMeadowDevice
+        /// Creates an <see cref="ISerialPort"/> directly from a <see cref="SerialPortName"/> using the current <see cref="IMeadowDevice"/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="baudRate"></param>
@@ -27,11 +27,11 @@ namespace Meadow
             int readBufferSize = 1024)
         {
             return name.SerialController?.CreateSerialPort(name, baudRate, dataBits, parity, stopBits, readBufferSize)
-                ?? throw new Exception("PortName does not have a valid serial controller");
+                ?? throw new Exception($"Port {name} does not have a valid serial controller");
         }
 
         /// <summary>
-        /// Creates an ISerialMessagePort directly from a SerialPortName using the current IMeadowDevice
+        /// Creates an <see cref="ISerialMessagePort"/> directly from a <see cref="SerialPortName"/> using the current <see cref="IMeadowDevice"/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="suffixDelimiter"></param>
@@ -53,23 +53,23 @@ namespace Meadow
             int readBufferSize = 512)
         {
             return name.SerialMessageController?.CreateSerialMessagePort(name, suffixDelimiter, preserveDelimiter, baudRate, dataBits, parity, stopBits, readBufferSize)
-                ?? throw new Exception("PortName does not have a valid serial controller");
-        }
+				?? throw new Exception($"Port {name} does not have a valid serial controller");
+		}
 
-        /// <summary>
-        /// Creates an ISerialMessagePort directly from a SerialPortName using the current IMeadowDevice
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="prefixDelimiter"></param>
-        /// <param name="preserveDelimiter"></param>
-        /// <param name="messageLength"></param>
-        /// <param name="baudRate"></param>
-        /// <param name="dataBits"></param>
-        /// <param name="parity"></param>
-        /// <param name="stopBits"></param>
-        /// <param name="readBufferSize"></param>
-        /// <returns></returns>
-        public static ISerialMessagePort CreateSerialMessagePort(
+		/// <summary>
+		/// Creates an <see cref="ISerialMessagePort"/> directly from a <see cref="SerialPortName"/> using the current <see cref="IMeadowDevice"/>
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="prefixDelimiter"></param>
+		/// <param name="preserveDelimiter"></param>
+		/// <param name="messageLength"></param>
+		/// <param name="baudRate"></param>
+		/// <param name="dataBits"></param>
+		/// <param name="parity"></param>
+		/// <param name="stopBits"></param>
+		/// <param name="readBufferSize"></param>
+		/// <returns></returns>
+		public static ISerialMessagePort CreateSerialMessagePort(
             this SerialPortName name,
             byte[] prefixDelimiter,
             bool preserveDelimiter,
@@ -81,7 +81,7 @@ namespace Meadow
             int readBufferSize = 512)
         {
             return name.SerialMessageController?.CreateSerialMessagePort(name, prefixDelimiter, preserveDelimiter, messageLength, baudRate, dataBits, parity, stopBits, readBufferSize)
-                ?? throw new Exception("PortName does not have a valid serial controller");
-        }
-    }
+				?? throw new Exception($"Port {name} does not have a valid serial controller");
+		}
+	}
 }
