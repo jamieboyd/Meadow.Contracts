332dcfa8200c1917629dcf09887613520e183456
Chris Tacke <ctacke@opennetcf.com>
2023-10-24 10:41:32 -0500

    added pots and rheostat interfaces

--- Source/Meadow.Contracts/Peripherals/ITouchScreen.cs
+++ Source/Meadow.Contracts/Peripherals/ITouchScreen.cs
@@ -1,28 +1,27 @@
-﻿namespace Meadow.Hardware
+﻿namespace Meadow.Hardware;
+
+/// <summary>
+/// An event delegate for touchscreen events
+/// </summary>
+/// <param name="x">The horizontal position of the touch</param>
+/// <param name="y">the vertical position of the touch </param>
+public delegate void TouchEventHandler(int x, int y);
+
+/// <summary>
+/// Represents a touch screen device
+/// </summary>
+public interface ITouchScreen
 {
     /// <summary>
-    /// An event delegate for touchscreen events
+    /// Event fired when the touchscreen is pressed
     /// </summary>
-    /// <param name="x">The horizontal position of the touch</param>
-    /// <param name="y">the vertical position of the touch </param>
-    public delegate void TouchEventHandler(int x, int y);
-
+    public event TouchEventHandler TouchDown;
     /// <summary>
-    /// Represents a touch screen device
+    /// Event fired when the touchscreen is released
     /// </summary>
-    public interface ITouchScreen
-    {
-        /// <summary>
-        /// Event fired when the touchscreen is pressed
-        /// </summary>
-        public event TouchEventHandler TouchDown;
-        /// <summary>
-        /// Event fired when the touchscreen is released
-        /// </summary>
-        public event TouchEventHandler TouchUp;
-        /// <summary>
-        /// Event fired when a cycle of press and release has occurred
-        /// </summary>
-        public event TouchEventHandler TouchClick;
-    }
+    public event TouchEventHandler TouchUp;
+    /// <summary>
+    /// Event fired when a cycle of press and release has occurred
+    /// </summary>
+    public event TouchEventHandler TouchClick;
 }
