b13eb22cc94aebcc57cab7430ec5b2c0661f4ff3
Adrian Stevens <adrian.stevens@gmail.com>
2023-12-31 14:10:19 -0800

    Replace IConcentationSensor with specific CO2/Ethanol/VOC contracts

---
+++ Source/Meadow.Contracts/Peripherals/Sensors/Environmental/IEnthanolConcentrationSensor.cs
@@ -0,0 +1,14 @@
+﻿using Meadow.Units;
+
+namespace Meadow.Peripherals.Sensors.Environmental;
+
+/// <summary>
+/// Ethanol Concentration interface requirements.
+/// </summary>
+public interface IEthanolConcentrationSensor : ISamplingSensor<Concentration>
+{
+    /// <summary>
+    /// Last value read from the Ethanol Concentration sensor.
+    /// </summary>
+    Concentration? EthanolConcentration { get; }
+}
