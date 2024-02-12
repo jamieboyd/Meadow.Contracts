54aeee1a7e3099fc7e594c9a8fc7e629402497b8
Eduardo Menezes <eduardomenezesm@msn.com>
2024-02-12 11:38:20 -0300

    Add support for Ethernet connection events

---
+++ Source/Meadow.Contracts/Hardware/Networking/EthernetNetworkConnectionEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Meadow.Hardware;
+
+/// <summary>
+/// Data relating to a Ethernet connection.
+/// </summary>
+public class EthernetNetworkConnectionEventArgs : NetworkConnectionEventArgs
+{
+    /// <summary>
+    /// Construct a EthernetNetworkConnectionEventArgs request.
+    /// </summary>
+    /// <param name="ipAddress">IP address of the device.</param>
+    /// <param name="subnet">Subnet of the device.</param>
+    /// <param name="gateway">Gateway address of the device.</param>
+    public EthernetNetworkConnectionEventArgs(IPAddress ipAddress, IPAddress subnet, IPAddress gateway)
+        : base(ipAddress, subnet, gateway)
+    {
+    }
+}
