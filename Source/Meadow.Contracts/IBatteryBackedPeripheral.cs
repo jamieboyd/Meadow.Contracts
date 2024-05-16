5e71ba90a84127b8d0682159c87866aa1914019d
Chris Tacke <ctacke@opennetcf.com>
2024-05-16 16:50:44 -0500

    add IBatteryBackedPeripheral interface

---
+++ Source/Meadow.Contracts/IBatteryBackedPeripheral.cs
@@ -0,0 +1,13 @@
+﻿namespace Meadow;
+
+/// <summary>
+/// Provides an abstraction for peripherals that have a battery and that can read information about that battery
+/// </summary>
+public interface IBatteryBackedPeripheral
+{
+    /// <summary>
+    /// Reads the state of the battery
+    /// </summary>
+    /// <returns><b>True</b> if the battery voltage is low, otherwise <b>False</b></returns>
+    bool IsBatteryLow();
+}
